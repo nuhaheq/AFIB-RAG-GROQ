82ae907e4800a23841fea6d82e239c8a62046902
NURIN NUHA BINTI MOHD FAIZAL <a205190@siswa.ukm.edu.my>
2026-09-21 01:54:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ ANSWER:
                 api_key = st.secrets["GROQ_API_KEY"]
                 client = Groq(api_key=api_key)
                 completion = client.chat.completions.create(
-                    model="llama-3.1-70b-versatile",  # tukar sini kalau nak model Groq lain
+                    model="qwen/qwen3.6-27b",  # tukar sini kalau nak model Groq lain
                     messages=[{"role": "user", "content": system_prompt}],
                 )
                 answer_text = completion.choices[0].message.content
