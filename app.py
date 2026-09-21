787816981b2270071dcc6ad06c0e1521728c3514
NURIN NUHA BINTI MOHD FAIZAL <a205190@siswa.ukm.edu.my>
2026-09-21 09:09:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ ANSWER:
                     "llama-3.3-70b-versatile",
                     "llama-3.1-8b-instant",
                     "openai/gpt-oss-120b",
-                    "openai/gpt-oss-20b"
+                    "gpt-oss"
                 ]
                 
                 answer_text = None
